f3274909bd51a2056c497b6972765ec1e1c5c6c5
Josep Roure Alcobé <josep@MacBook-MacBook-Pro-de-Josep.local>
2017-02-07 12:24:50 +0100

    first commit. Project with three modules

---
+++ reverseproxy/src/main/java/cat/tecnocampus/ReverseproxyApplication.java
@@ -0,0 +1,12 @@
+package cat.tecnocampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReverseproxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReverseproxyApplication.class, args);
+	}
+}
